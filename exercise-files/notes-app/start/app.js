d00942b194450c1e4bc6bcb168e10b272df8652d
johannlilly <johannlilly@users.noreply.github.com>
2020-02-06 16:24:19 -0500

    create `yargs` commands to handle args

--- exercise-files/notes-app/start/app.js
+++ exercise-files/notes-app/start/app.js
@@ -2,4 +2,44 @@ const chalk = require('chalk');
 const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
+// Customize yargs version
+yargs.version('1.0.0');
+
+// Create add command
+yargs.command({
+  // options object to customize how the command will work
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () { // runs upon calling command
+    console.log('Adding a new note!');
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function() {
+    console.log('Removing the note')
+  }
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function() {
+    console.log('Reading the note')
+  }
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List notes',
+  handler: function() {
+    console.log('Listing notes')
+  }
+});
+
 console.log(yargs.argv);
