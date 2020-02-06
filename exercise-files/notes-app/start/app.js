74fd0f73a97a0b425cfe42e8acaaed9a36dbf326
johannlilly <johannlilly@users.noreply.github.com>
2020-02-06 16:04:34 -0500

    handle argument passed to app

--- exercise-files/notes-app/start/app.js
+++ exercise-files/notes-app/start/app.js
@@ -1,11 +1,10 @@
-const validator = require('validator');
-const getNotes = require('./notes.js');
 const chalk = require('chalk');
+const getNotes = require('./notes.js');
 
-const msg = getNotes();
-console.log(msg);
+const command = process.argv[2];
 
-console.log(validator.isURL('https://github.com'));
-
-const greenMsg = chalk.blue.inverse.bold('Success');
-console.log(greenMsg);
+if (command === 'add') {
+  console.log('Adding note!');
+} else if (command === 'remove') {
+  console.log('Removing note!');
+}
