17e70945d354f6853214b1cf97147a76424f2ece
johannlilly <johannlilly@users.noreply.github.com>
2020-02-06 16:17:40 -0500

    Argument Parsing with `Yargs`
    handle flags passed with arguments

--- exercise-files/notes-app/start/app.js
+++ exercise-files/notes-app/start/app.js
@@ -1,10 +1,5 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const command = process.argv[2];
-
-if (command === 'add') {
-  console.log('Adding note!');
-} else if (command === 'remove') {
-  console.log('Removing note!');
-}
+console.log(yargs.argv);
