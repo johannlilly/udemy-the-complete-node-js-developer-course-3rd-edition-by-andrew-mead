0d511fa4894fc0b7e597c71e57f611ee293abdc7
johannlilly <johannlilly@users.noreply.github.com>
2020-02-06 15:18:27 -0500

    `chalk` lib demo

--- exercise-files/notes-app/start/app.js
+++ exercise-files/notes-app/start/app.js
@@ -1,5 +1,10 @@
+const validator = require('validator');
 const getNotes = require('./notes.js');
+const chalk = require('chalk');
 
 const msg = getNotes();
-
 console.log(msg);
+
+console.log(validator.isURL('https://github.com'));
+
+console.log(chalk.green('Success!'));
