4547c45bb3b050c93dc06ec3dfcfda2198a1f4c3
johannlilly <johannlilly@users.noreply.github.com>
2020-02-06 13:29:42 -0500

    `fs.appendFileSync` append data to file

--- exercise-files/notes-app/start/app.js
+++ exercise-files/notes-app/start/app.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'My name is Vengeance!');
+fs.writeFileSync('notes.txt', 'My name is Vengeance.');
+
+fs.appendFileSync('notes.txt', '\nI am the night.');
