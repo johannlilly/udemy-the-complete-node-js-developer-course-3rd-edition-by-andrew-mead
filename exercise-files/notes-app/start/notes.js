b8598543535543a1152ee2338612692edf675f16
johannlilly <johannlilly@users.noreply.github.com>
2020-02-07 11:57:56 -0500

    handle create new note with `fs`

--- exercise-files/notes-app/start/notes.js
+++ exercise-files/notes-app/start/notes.js
@@ -1,9 +1,19 @@
+const fs = require('fs');
+
 const getNotes = function() {
   return 'Your notes...';
 }
 
 const addNote = function (title, body) {
-  
+  const notes = loadNotes();
+}
+
+const loadNotes = function() {
+
+  // return array of notes
+  const dataBuffer = fs.readFileSync('notes.json');
+  const dataJSON = dataBuffer.toString();
+  return JSON.parse(dataJSON);
 }
 
 module.exports = {
